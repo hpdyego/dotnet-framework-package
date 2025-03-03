eeca74cd979577b1839752e86bf88c958e1450f2
Dyego Dias <hpdyego@gmail.com>
2025-03-03 12:17:35 -0300

    Adiciona a função de subtração

--- Classes/Calculadora.cs
+++ Classes/Calculadora.cs
@@ -3,5 +3,7 @@ namespace dotnetFrameworkPackage
     public class Calculadora
     {
         public int Somar(int a, int b) => a + b;
+
+        public int Subtrair(int a, int b) => a - b;
     }
 }
